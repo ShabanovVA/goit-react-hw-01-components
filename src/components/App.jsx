0ecead98d7f95a8ae929f42e7628d4d491329be0
ShabanovVA <1514768zp@gmail.com>
2023-03-18 00:38:57 +0200

    HW2 in work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from "components/Profile/Profile";
-import user from "../user.json";
+import {Statistics} from "components/Statistics/Statistics"
+import user from "./Profile/user.json";
+import data from "./Statistics/data.json";
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
-    </div>
+      <Statistics title="Upload stats" data={data}/></div>
   );
 };

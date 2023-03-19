9b7673b5e9e19a50b9a7b10afa3bc29b87937dce
ShabanovVA <1514768zp@gmail.com>
2023-03-19 20:45:23 +0200

    TransactionHistory - ready.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { Profile } from "components/Profile/Profile";
 import { Statistics } from "components/Statistics/Statistics"
 import {FriendList} from 'components/FriendList/FriendList'
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
 import user from "./Profile/user.json";
 import data from "./Statistics/data.json";
 import friends from "./FriendList/friends.json";
+import transactions from "./TransactionHistory/transactions.json";
+
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends}/></div>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
   );
 };

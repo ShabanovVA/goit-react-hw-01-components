f8537a1bd2bbed647f22dd1fc78a33dedcef2f2f
ShabanovVA <1514768zp@gmail.com>
2023-03-20 20:50:38 +0200

    Update.App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import transactions from "./TransactionHistory/transactions.json";
 export const App = () => {
   return (
     <div>
-      <Profile username={user.username} tag={user.tag} avatar={user.avatar} stats={user.stats} />
+      <Profile username={user.username} tag={user.tag} avatar={user.avatar} stats={user.stats} location={user.location} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
